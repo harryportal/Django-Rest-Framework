1e068d3be54f3997b635633017e2f56fd8e2516d
harry <dammykuye@gmail.com>
2022-06-21 21:46:41 +0100

    class based views

--- TodoApi/snippets/urls.py
+++ TodoApi/snippets/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'snippets'
 
 urlpatterns = [
-    path('snippets', views.snippet_list, name='snippets_detail'),
-    path('snippets/<int:pk>', views.snippet_detail, name='snippets_detail')
+    path('snippets', views.snippet_list.as_view(), name='snippets_detail'),
+    path('snippets/<int:pk>', views.snippet_detail.as_view(), name='snippets_detail')
 
 ]
